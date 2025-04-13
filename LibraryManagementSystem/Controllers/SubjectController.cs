844a9411ebd55929a0cfbc82c1bee44f7c47eacc
Duan <daniel.w2301@gmail.com>
2025-04-12 21:46:28 -0500

    add authorize and modify view

--- LibraryManagementSystem/Controllers/SubjectController.cs
+++ LibraryManagementSystem/Controllers/SubjectController.cs
@@ -1,26 +1,26 @@
 ﻿using LibraryManagementSystem.BLL;
 using LibraryManagementSystem.Model;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryManagementSystem.Controllers
 {
-    public class SubjectsController : Controller
+    [Authorize(Roles = "Librarian")]
+    public class SubjectController : Controller
     {
         private readonly SubjectService _service;
 
-        public SubjectsController(SubjectService service)
+        public SubjectController(SubjectService service)
         {
             _service = service;
         }
 
-        // GET: Subjects
         public IActionResult Index()
         {
             var subjects = _service.GetAll();
             return View(subjects);
         }
 
-        // GET: Subjects/Details/5
         public IActionResult Details(int id)
         {
             var subject = _service.GetById(id);
@@ -30,13 +30,11 @@ namespace LibraryManagementSystem.Controllers
             return View(subject);
         }
 
-        // GET: Subjects/Create
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Subjects/Create
         [HttpPost]
         public IActionResult Create(Subject subject)
         {
@@ -48,7 +46,6 @@ namespace LibraryManagementSystem.Controllers
             return View(subject);
         }
 
-        // GET: Subjects/Edit/5
         public IActionResult Edit(int id)
         {
             var subject = _service.GetById(id);
@@ -58,7 +55,6 @@ namespace LibraryManagementSystem.Controllers
             return View(subject);
         }
 
-        // POST: Subjects/Edit/5
         [HttpPost]
         public IActionResult Edit(int id, Subject subject)
         {
@@ -73,7 +69,6 @@ namespace LibraryManagementSystem.Controllers
             return View(subject);
         }
 
-        // GET: Subjects/Delete/5
         public IActionResult Delete(int id)
         {
             var subject = _service.GetById(id);
@@ -83,7 +78,6 @@ namespace LibraryManagementSystem.Controllers
             return View(subject);
         }
 
-        // POST: Subjects/Delete/5
         [HttpPost]
         public IActionResult DeleteConfirmed(int id)
         {
